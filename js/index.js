696f353efd5f6f277b67215c0ff1d63b865ca32c
Dluoluo <luo.jiami@qq.com>
2018-05-26 00:00:27 +0800

    '大概完结'

--- js/index.js
+++ js/index.js
@@ -2,7 +2,22 @@ var myWendaSwiper = new Swiper('.swiper-wenda', {
     // 垂直
     direction: 'vertical',
     // 不重复
-    loop: false
+    loop: false,
+    
+    onInit: function (swiper) {
+        slide = swiper.slides.eq(0);
+        slide.addClass('ani-slide');
+    }, onTransitionStart: function (swiper) {
+        for (i = 0; i < swiper.slides.length; i++) {
+            slide = swiper.slides.eq(i);
+            slide.removeClass('ani-slide');
+        }
+    }, onTransitionEnd: function (swiper) {
+        slide = swiper.slides.eq(swiper.activeIndex);
+        slide.addClass('ani-slide');
+
+    },
+
 }) 
 $('.rankings-list').mCustomScrollbar({
     axis: "y",
