d81d4623e59083e5191fcecf6e487361977f0c22
Dluoluo <luo.jiami@qq.com>
2018-05-25 16:55:32 +0800

    '排行耪+问答样式'

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ var myWendaSwiper = new Swiper('.swiper-wenda', {
     // 不重复
     loop: false
 }) 
+$('.rankings-list').mCustomScrollbar({
+    axis: "y",
+    setWidth:"548px",
+    setHeight:"450px",
+    scrollbarPosition: "outside",
+    scrollButtons: { enable: true }
+})
